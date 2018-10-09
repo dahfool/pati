3edc3ee825b51e7c6400f527c4e966663344b90d
Gerald Isabelle <info@geraldisabelle.me>
2018-10-09 23:41:55 +0100

    add dependencies, .babelrc and config files to make tests work on webstorm

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.js?(x)',
+    '<rootDir>/src/**/?(*.)(spec|test).js?(x)'
+  ],
+  transform: {
+    '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js'
+  }
+}
